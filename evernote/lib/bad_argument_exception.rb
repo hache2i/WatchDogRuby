6e3cd4eba2767c39c1b831b640c0040495c5695c
Victor Miguez <olareoun@olareoun-mba-ubuntu.(none)>
2013-07-20 18:53:39 +0200

    delete other project files

--- evernote/lib/bad_argument_exception.rb
+++
@@ -1,6 +0,0 @@
-class BadArgumentException < Exception
-	attr_reader :exception_key
-	def initialize(exception_key)
-		@exception_key = exception_key
-	end
-end
--- notebooks/lib/notebook_not_found_exception.rb
+++
@@ -1,2 +0,0 @@
-class NotebookNotFoundException < Exception
-end
