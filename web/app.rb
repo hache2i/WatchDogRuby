03d03a9cc47de7bbfa3b4d1b30935eb9eb11a658
Victor Miguez <olareoun@olareoun-mba-ubuntu.(none)>
2013-07-30 11:52:24 +0200

    experimenting

--- web/app.rb
+++ web/app.rb
@@ -8,7 +8,7 @@ require_relative './lib/notifier'
 require_relative '../files/lib/files_domain'
 require_relative '../files/lib/files_to_change'
 require_relative '../users/lib/users_domain'
-require 'rufus-scheduler'
+
 require 'gapps_openid'
 require 'rack/openid'
 require_relative './lib/google_util'
