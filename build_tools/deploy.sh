c02be4f0cea26676dbff6f2b5b3d4be834893c0d
= <=>
2013-09-24 14:01:46 +0200

    improving deploy proccess

--- build_tools/deploy.sh
+++ build_tools/deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo "creating brach and deploying to watchdog-hache2i-heroku"
+git checkout -b local_temp
+git add .
+git commit -m "local changes not going to production"
 git checkout master
 git pull origin master
-git branch -D deploy
 git checkout -b deploy
 git push -f heroku deploy:master
 git reset --hard
 git checkout master
+git merge local_temp
+git branch -D local_temp
 git branch -D deploy
