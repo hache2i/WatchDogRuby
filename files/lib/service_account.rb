1bf7b2a40f647f08e58ad95f2cbbcb0638ebd202
olareoun <victor.miguez.martinez@gmail.com>
2014-08-21 17:05:45 +0200

    new service account data and key

--- files/lib/service_account.rb
+++ files/lib/service_account.rb
@@ -2,10 +2,10 @@ require 'google/api_client'
 
 class ServiceAccount
 	def initialize
-		keyFile = File.read(File.join(File.dirname(__FILE__), '5b198e954f9735c6cbc2c28dfe79b9cf8c90e909-privatekey.p12'))
+		keyFile = File.read(File.join(File.dirname(__FILE__), 'API-Project-6f4a3c23ba4d.p12'))
 		key = Google::APIClient::PKCS12.load_key(keyFile, 'notasecret')
 		@service_account = Google::APIClient::JWTAsserter.new(
-		    '622425308764-du41u0dqopb3eja3ei9s2q4v30mihuie@developer.gserviceaccount.com',
+		    '95351256687-0mmci6rtdlslqv839l8ah7f3fqp10qu4@developer.gserviceaccount.com',
 		    ['https://www.googleapis.com/auth/admin.directory.user', 'https://www.googleapis.com/auth/drive'],
 		    key)
 	end
