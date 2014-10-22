7f6a6cfae0bade5158f4f244c2f25f4d99a433b2
olareoun <victor.miguez.martinez@gmail.com>
2014-10-22 09:43:02 +0200

    new service account data

--- files/lib/service_account.rb
+++ files/lib/service_account.rb
@@ -1,8 +1,8 @@
 require 'google/api_client'
 
 class ServiceAccount
-	P12 = "WATCHDOG-25642ef8c382.p12"
-	MAIL = "867950402393-nqa73qd0dh9u3g71pl25jpd3abeeg3sl@developer.gserviceaccount.com"
+	P12 = "WATCHDOG-122d396cea06.p12"
+	MAIL = "867950402393-r4chltcopuocui2abrlv1u2ph1h477a7@developer.gserviceaccount.com"
 	# P12 = "API-Project-6f4a3c23ba4d.p12"
 	# MAIL = "95351256687-0mmci6rtdlslqv839l8ah7f3fqp10qu4@developer.gserviceaccount.com"
 	def initialize
