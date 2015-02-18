e86090f1bb13102f0e0c228ea878af2574b87aaf
olareoun <victor.miguez.martinez@gmail.com>
2015-02-18 16:47:54 +0100

    debug log

--- files/lib/children_folders.rb
+++ files/lib/children_folders.rb
@@ -41,7 +41,7 @@ module Files
     end
 
     def exec
-      WDLogger.info "checking folder #{@folder.inspect}"
+      WDLogger.debug "checking folder #{@folder.inspect}"
       begin
         result = DriveApiHelper.list_files @driveConnection, assembleParams(getPageToken(result))
         break unless result.success?
