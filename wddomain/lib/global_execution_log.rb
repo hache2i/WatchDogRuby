db0e3b8781f5070231f5a657e49a18eaa8144128
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-03-24 09:56:50 +0100

    moving logic from global execution log to execution log, something like from interface to implementation

--- wddomain/lib/global_execution_log.rb
+++ wddomain/lib/global_execution_log.rb
@@ -4,28 +4,16 @@ module Watchdog
 			module Execution
 				extend self
 
-				DEFAULT_LEVELS = [:info, :error, :debug]
-				PAGE_SIZE = 25
-
 				def add msg, domain = nil, user = nil, level = nil
 					log.add msg, domain, user, level
 				end
 
 				def get
-					p "looking for records from scratch #{log.records.count}"
-					selected = log.records.select { |record| DEFAULT_LEVELS.include? record.level }
-					total_at_time = selected.size
-					records = selected.take PAGE_SIZE
-					{ records: records, total_at_time: total_at_time, from_scratch: true }
+					log.get
 				end
 
 				def get_from from, total_at_time
-					p "looking for records from #{from} of #{total_at_time} absolute total #{log.records.count}"
-					restart = from >= total_at_time
-					return get if restart
-					selected = log.records.select { |record| DEFAULT_LEVELS.include? record.level }
-					records = selected.slice selected.size - total_at_time + from, PAGE_SIZE
-					{ records: records, total_at_time: total_at_time }
+					log.get_from from, total_at_time
 				end
 
 				private
