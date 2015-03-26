e640783e1c60de82a048980ee5717761789db5a8
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-03-26 11:36:59 +0100

    adding log

--- files/lib/files_domain.rb
+++ files/lib/files_domain.rb
@@ -5,6 +5,7 @@ require_relative 'user_files_domain'
 require_relative 'user_files_exception'
 require_relative 'user_files_to_change'
 require_relative 'drive_api_helper'
+require_relative '../../wd_logger'
 
 module Files
 	class FilesDomain
@@ -18,7 +19,10 @@ module Files
 			domainFilesToChange.each do |userFilesToChange|
 				user = userFilesToChange.getEmail
 				userFilesDomain = UserFilesDomain.new @driveConnection, user, domain
-				user_files = Changed.find(userFilesToChange.getFiles.map {|file| file["id"]})
+				WDLogger.info "going to get ids"
+				files_ids = userFilesToChange.getFiles.map {|file| file["id"]}
+				WDLogger.info "going to get ids: #{files_ids.count}"
+				user_files = Changed.find()
 				WDLogger.info "Found #{user_files.count} for #{user}"
 				userFilesDomain.changeUserFilesPermissions user_files
 			end
