7301283be1302a7bbb406a951a5746010b55a37f
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-03-26 16:54:38 +0100

    removing traces

--- files/lib/files_domain.rb
+++ files/lib/files_domain.rb
@@ -19,11 +19,8 @@ module Files
 			domainFilesToChange.each do |userFilesToChange|
 				user = userFilesToChange.getEmail
 				userFilesDomain = UserFilesDomain.new @driveConnection, user, domain
-				WDLogger.info "going to get ids"
 				files_ids = userFilesToChange.getFiles.map {|file| file["id"]}
-				WDLogger.info "going to get ids: #{files_ids}"
 				user_files = Changed.find(files_ids)
-				WDLogger.info "Found #{user_files.count} for #{user}"
 				userFilesDomain.changeUserFilesPermissions user_files
 			end
 		end
