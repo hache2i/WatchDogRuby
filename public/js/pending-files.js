0f39c8844e6724c75d58cd858eca054a39b5e089
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-07-24 09:44:01 +0200

    more button on files table and getting rid of type column

--- public/js/pending-files.js
+++ public/js/pending-files.js
@@ -75,30 +75,23 @@
 					return React.createElement("tr", {},
 						React.createElement("td", {}, file.title),
 						React.createElement("td", {}, file.path),
-						React.createElement("td", {}, file.type),
 						React.createElement("td", {}, file.oldOwner),
 						React.createElement("td", {}, file.newOwner)
 					);
 				});
-				return React.createElement("table", { className: "table table-striped", id: "files" },
+				var filesTable = React.createElement("table", { className: "table table-striped", id: "files" },
 					React.createElement("thead", {},
 						React.createElement("tr", {},
 							React.createElement("td", {}, "Titulo"),
 							React.createElement("td", {}, "Path"),
-							React.createElement("td", {}, "Tipo"),
 							React.createElement("td", {}, "Antiguo Propietario"),
 							React.createElement("td", {}, "Nuevo Propietario")
 						)
 					),
 					React.createElement("tbody", {}, filesNodes)
 				);
-				// var filesNodes = this.props.files.map(function(file){
-				// 	var fileData = file.title + " ## " + file.oldOwner + " ## " + file.newOwner;
-				// 	return React.createElement("li", {}, fileData);
-				// });
-				// var listNode = React.createElement("ul", {}, filesNodes);
-				// var moreBtn = React.createElement("a", { onClick: this.handleClick }, "Más");
-				// return React.createElement("div", {}, listNode, moreBtn);
+				var moreBtn = React.createElement("a", { onClick: this.handleClick }, "Más");
+				return React.createElement("div", {}, filesTable, moreBtn);
 			}
 		});
 
