a7e75eb273b36897aa37ab75c3b9f54430bcef88
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-07-31 18:51:05 +0200

    counting only pending files

--- actions/get_pending_files_count.rb
+++ actions/get_pending_files_count.rb
@@ -4,8 +4,8 @@ module Wd
 
 			def self.do domain, filter
 				count = 0
-				count = Files::Changed.where(domain: domain).count if filter.nil?
-				count = Files::Changed.where(domain: domain).in("oldOwner" => filter["oldOwner"]).count unless filter.nil?
+				count = Files::Changed.where(domain: domain, pending: true).count if filter.nil?
+				count = Files::Changed.where(domain: domain, pending: true).in("oldOwner" => filter["oldOwner"]).count unless filter.nil?
 				{ count: count }
 			end
 
