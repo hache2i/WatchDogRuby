4f576b110b0a6358bb49933f4754ca3e0c6be31c
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-08-02 18:24:03 +0200

    multithreading to change permissions

--- actions/change_all_pending_files.rb
+++ actions/change_all_pending_files.rb
@@ -9,18 +9,23 @@ module Wd
 			def self.do domain, filter
 				users_with_pending_files = Files::Changed.users domain if filter.nil?
 				users_with_pending_files = filter["oldOwner"] unless filter.nil?
-				
+	
+				thrs = []			
+				Thread.abort_on_exception = true
 				users_with_pending_files.reverse.each do |user|
-					Thread.abort_on_exception = true
-					thrs = Thread.new {
+					thrs << Thread.new {
 						WDLogger.debug "started thread to change permissions for #{user}"
 						user_files = Files::Changed.pending_for_user user
 						userFilesDomain = Files::UserFilesDomain.new Files::DriveConnection.new, user, domain
-						userFilesDomain.changeUserFilesPermissions user_files
+						user_files.each_slice(100).to_a.each do |files|
+							Thread.new {
+								userFilesDomain.changeUserFilesPermissions files
+							}
+						end
 					}
 				end
 
-				thrs.each { |thr| thr.join }
+				# thrs.each { |thr| thr.join }
 			end
 		end
 	end
