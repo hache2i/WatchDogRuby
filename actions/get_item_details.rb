2c938c8dea2c9df439bbb83360caea4a35ac5fce
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-08-18 13:31:41 +0200

    improving details

--- actions/get_item_details.rb
+++ actions/get_item_details.rb
@@ -7,12 +7,19 @@ module Wd
         driveConnection.authorize(user)
 
         result = Files::DriveApiHelper.get_item driveConnection, itemId
-
+        item_data = result.data
         changes = Files::Changed.where fileId: itemId
 
+        parent_change = {}
+        if item_data.parents.count == 1
+          parent_id = item_data.parents[0].id
+          parent_change = Files::Changed.where fileId: parent_id
+        end
+
         { 
-          drive_data: result.data,
-          changes_data: changes
+          drive_data: item_data,
+          changes_data: changes,
+          parent_change: parent_change
         }
       end
 
