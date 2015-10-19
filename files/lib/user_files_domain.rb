cd03ed568593338b3b434ad2b4b838c229a41431
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-10-19 20:35:34 +0200

    traces

--- files/lib/user_files_domain.rb
+++ files/lib/user_files_domain.rb
@@ -28,6 +28,7 @@ module Files
 				p change_proposal.inspect
 				change_proposal.update_attributes!(pending: false, executed: Time.now.to_i)
 				p change_proposal.inspect
+				p Changed.find(change_proposal.id)
 			else
 				p "__________________change permissions - fail"
 				WDLogger.error("(¡ FALLO !) cambio de propiedad '#{file.title}' - #{api_result[:status].to_s}", @domain, @user)
